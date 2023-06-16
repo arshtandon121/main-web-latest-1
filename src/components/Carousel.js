02794c43007c8582a19882a4652d73688adc1f5a
Arsh <arshtandon121@gmail.com>
2023-06-15 18:33:36 -0700

    crouser fixed navbar issue

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,31 +1,24 @@
-import React from 'react'
-import meta from "../Assets/meta.png"
-
+import React from "react";
+import meta from "../Assets/meta.png";
 
 export default function Carousel() {
   return (
     <div>
-      <div className='carousel-component'>
-
-      <h3 className='carousel-heading'>Brands we have worked with</h3>
-
+      <div className="carousel-component">
+        <h3 className="carousel-heading">Brands we have worked with</h3>
       </div>
-
-      <div className="carousel">
-
-   
-
-        <div className="logo">
-          <img src={meta} />
-          <img src={meta} />
-          <img src={meta} />
-          <img src={meta} />
-          <img src={meta} />
-          <img src={meta} />
-    
-          
+      <div className="cr1">
+        <div className="carousel">
+          <div className="logo">
+            <img src={meta} />
+            <img src={meta} />
+            <img src={meta} />
+            <img src={meta} />
+            <img src={meta} />
+            <img src={meta} />
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
